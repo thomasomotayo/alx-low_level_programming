28e6a777cc8d31229020dc53627a6133ac472bb0
Thomas Omotayo <thomasomotayo@gmail.com>
2022-10-13 08:35:34 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-// program to to print on the screen
+/**
+ *   main - Entry point
+ *
+ *     Return: Always 0 (Success)
+ 
+  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
