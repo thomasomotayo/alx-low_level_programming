e2d52c46b29f568e11f6889d116c82d7f3bf114e
Thomas Omotayo <thomasomotayo@gmail.com>
2022-10-14 19:52:43 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: returns 0 when everything works fine
+*
+*/
+
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; ++n)
+	{
+		putchar(n);
+	}
+	putchar(10);
+	return (0);
+}
+Footer
+
