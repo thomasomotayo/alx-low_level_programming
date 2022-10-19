9d908a188fa6f27ccb6381f3809d504afdbfb4b3
Thomas Omotayo <thomasomotayo@gmail.com>
2022-10-19 13:13:21 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,46 +1,18 @@
-#include "holberton.h"
-
 #include "main.h"
-
-	/**
-
-	 * main - Entry point
-
-	 *
-
-	 * Return: Always 0 (Success)
-
-	 */
-
-	int main(void)
-
-	{
-
-		char *sh = "Holberton";
-
-	
-
-
-
-		while (*sh)
-
-		{
-
-			_putchar(*sh);
-
-			sh++;
-
-		}
-
-		_putchar('\n');
-
-	
-
-
-
-		return (0);
-
-	}
-
-
-
+/**
+ * main - print _putchar
+ * is written by bwave
+ * Return: 0
+ */
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putcahr('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
