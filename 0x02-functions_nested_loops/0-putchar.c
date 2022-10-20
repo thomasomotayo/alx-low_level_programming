b443e2e62fa0238c1a33a0cacc09ee27b1adf976
Thomas Omotayo <thomasomotayo@gmail.com>
2022-10-19 17:34:51 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * main - print _putchar
  * is written by bwave
@@ -7,15 +8,15 @@
 
 int main(void)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putcahr('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
-return(0);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putcahr('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return(0);
 }
 
