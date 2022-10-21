8b012aac13304895aa83f0f15458332d6838f23e
Thomas Omotayo <thomasomotayo@gmail.com>
2022-10-21 04:02:22 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isuper - function that check the uppercase character
+ * 0c: input
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+int _isupper(int c);
+{
+if((c >= 'A') && (c <= 'Z'))
+return (1);
+else
+retuen (0)
+}
