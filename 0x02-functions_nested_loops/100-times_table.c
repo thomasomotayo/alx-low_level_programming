5b3f588c70f2d9d15c0e56c1de4c97f985bc9dfd
Thomas Omotayo <thomasomotayo@gmail.com>
2022-10-23 13:31:36 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,18 +9,37 @@
  */
 int main(void)
 {
-	int i, sum = 0;
+	int num, mult, prod;
 
-	for (i = 0; i < 1024; i++)
+	if (n >= 0 && n <= 15)
 	{
-		if ((i % 3) == 0 || (i % 5) == 0)
-			sum += i;
+		for (num = 0; num <= n; num++)
+		{
+			_putchar('0');
+			for (mult = 1; mult <= n; mult++)
+			{
+				_putchar(',');
+				_putchar(' ');
 
+				prod = num * mult;
+				if (prod <= 99)
+					_putchar(' ');
+				if (prod <= 9)
+					_putchar(' ');
+
+				if (prod >= 100)
+				{
+					_putchar((prod / 100) + '0');
+					_putchar(((prod / 10)) % 10 + '0');
+				}
+				else if (prod <= 99 && prod >= 10)
+				{
+					_putchar((prod / 10) + '0');
+				}
+				_putchar((prod % 10) + '0');
+			}
+			_putchar('\n');
+		}
 	}
-
-
-	printf("%d\n", sum);
-
-
-	return (0);
 }
+
