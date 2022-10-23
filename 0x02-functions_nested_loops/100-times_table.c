909b815d37096eb7ceb5a6ac97f0d15f4397ce02
Thomas Omotayo <thomasomotayo@gmail.com>
2022-10-23 12:05:04 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -11,19 +11,18 @@
 
 void times_table(void)
 {
-	int num, mult, prod, n;
-	if (n >= 0 && n <= 15)
-	{
-		for (num = 0; num <= n; num++)
+	int num, mult, prod;
+	
+		for (num = 0; num <= 9; num++)
 		{
 			_putchar('0');
 
-			for (mult = 1; mult <= n; mult++)
+			for (mult = 1; mult <= 9; mult++)
 			{
 				_putchar(',');
 				_putchar(' ');
 				prod = num * mult;
-				if (prod <= 99)
+				if (prod <= 9)
 					_putchar(' ');
 				if (prod <= 9)
 					_putchar(' ');
