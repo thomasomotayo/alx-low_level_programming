f01a3e210b9c7a7eadb799fc361e94c6df833699
Thomas Omotayo <thomasomotayo@gmail.com>
2022-10-23 10:53:27 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,15 +11,12 @@
 void print_to_98(int n)
 
 {
-
 	if (n < 98)
 
 	{
-
 		while (n <= 98)
 
 		{
-
 			printf("%d", n);
 
 			if (n != 98)
@@ -27,45 +24,33 @@ void print_to_98(int n)
 			{
 
 				printf(", ");
-
 			}
-
 			n++;
-
 		}
-
 	}
 else if (n > 98)
 
 	{
-
 		while (n >= 98)
 
 		{
-
 			printf("%d", n);
 
 			if (n != 98)
 
 			{
-
 				printf(", ");
 
 			}
 
 			n--;
-
 		}
-
 	}
 else
-
 	{
 
 		printf("98");
-
 	}
 
 	printf("\n");
-
 }
