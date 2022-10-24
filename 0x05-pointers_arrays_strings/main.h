3db5e1b8133c82919aadbc11371d85757fef9f1d
Thomas Omotayo <thomasomotayo@gmail.com>
2022-10-24 08:44:39 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -8,6 +8,7 @@
 
 #include <stdlib.h>
 
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -18,5 +19,6 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
+int _atol(char *s);
 
 #endif
