c64c52d11e74761546ab0298088e6414cedc03b1
Thomas Omotayo <thomasomotayo@gmail.com>
2022-10-24 18:15:18 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,10 +9,27 @@
 
 int _atoi(char *s)
 {
-	 int i;
-	 s = " -9885";
-	 i = atoi(s); 
-	_putchar("i = %d\n",i);
+	int signs = 1;
+	unsigned int sum = 0;
+	int final = 0;
 
-	return (0);
+	for (; *s != '\0'; s++)
+	{
+		if (*s == '-')
+			signs *= -1;
+		else if (*s >= '0' && *s <= '9')
+			break;
+	}
+	while (*s >= '0' && *s <= '9')
+	{
+		sum *= 10;
+		sum += *s - '0';
+		s++;
+	}
+	if (signs == -1)
+		final = -sum;
+	else
+		final = sum;
+	return (final);
 }
+	
