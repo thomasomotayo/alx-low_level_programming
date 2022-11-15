4bcbc856684003478e04abc99310d215caf3fbe1
Thomas Omotayo <thomasomotayo@gmail.com>
2022-11-14 18:11:32 -0800

    abuse

---
+++ 0x0D-preprocessor/101-preprocessor_abuse.c
@@ -0,0 +1,17 @@
+#ifndef _HEADER_
+#define _HEADER_
+#define TEXT "Hello, Omotayo Thomas"
+#endif
+#include <stdio.h>
+
+/**
+  * main - ...
+  *
+  * Return: ...
+  */
+
+int main(void)
+{
+	printf("%s\n", TEXT);
+	return (0);
+}
