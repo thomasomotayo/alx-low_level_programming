9edc42de26d7732d9b2368fde87c04d77c9c0ea2
Thomas Omotayo <thomasomotayo@gmail.com>
2022-12-05 20:42:57 -0800

    task1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,6 +1,8 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+
+
 /**
  * create_file - function that creates a file.
  * @filename: variable pointer
@@ -33,4 +35,4 @@ int create_file(const char *filename, char *text_content)
 	write(file, text_content, i);
 
 	return (1);
-
+}
